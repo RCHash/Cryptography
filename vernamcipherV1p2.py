6d500a6b6b17bb452881db2c2a40abc072152576
RCHash <ricardocavanha@live.com>
2022-03-20 03:58:27 -0300

    minor comment fixes

--- vernamcipherV1p2.py
+++ vernamcipherV1p2.py
@@ -22,10 +22,10 @@ def vernam_encrypt(plaintext):
     #creates a key big enough to encompass the whole plaintext
     binkeytemp=[];
     while len(binkeytemp)<len(binplaintext):
-        #generate a random key and hash it
+        #generates a random key and hashes it
         #sha256 is a pseudorandom function that requires a string formated to utf-8 and outputs ???
         #encode is a method that allows encoding in specific formats, such as utf-8
-        #hexdigest is a method that returns the data converted to hexadecimal
+        #hexdigest is a method that returns the data converted to hexadecimal (the first two characters returned are an indication of a hexadecimal number)
         #int converts the hexadecimal from base 16 to base 10
         key=int(sha256(str(urandom(BLOCKSIZE)).encode('utf-8')).hexdigest()[2:],16);
         #convert the key to binary
