1e06f54ea25f4c2f8b40aeae6454914a97151a02
RCHash <ricardocavanha@live.com>
2022-04-11 14:11:54 -0300

    keygen development

---
+++ keygen.py
@@ -0,0 +1,22 @@
+from os import urandom;
+from hashlib import sha256;
+
+#generates a pseudorandom key of length kLength
+def keygen(kLength):
+    """Generates a pseudorandom key of length kLength
+    Uses a nonce to increase the output of the key to the required length
+    -Assumes kLength is an integer>0
+    Returns a key as a String"""
+    #get the code from own github account
+    binkeytemp=[];
+    key="";
+    while len(str(key))<kLength:
+        #generates a random key and hashes it
+        #sha256 is a pseudorandom function that requires a string formated to utf-8 and outputs ???
+        #encode is a method that allows encoding in specific formats, such as utf-8
+        #hexdigest is a method that returns the data converted to hexadecimal (the first two characters returned are an indication of a hexadecimal number)
+        #int converts the hexadecimal from base 16 to base 10
+        key=str(sha256(str(urandom(BLOCKSIZE)).encode('utf-8')).hexdigest()[2:]);
+    #truncate the key to have the desired length
+    fkey=''.join(key[k] for k in range(kLength));
+    return fkey;
