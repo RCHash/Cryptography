27ac8f25e37846a98400f3e1f71f3cb30435a6b0
RCHash <ricardocavanha@live.com>
2022-04-16 07:39:08 -0300

    randomness option implemented

--- keygen.py
+++ keygen.py
@@ -2,12 +2,13 @@ from os import urandom;
 from hashlib import sha256;
 
 #generates a pseudorandom key of length kLength with an optional seed
-def keygen(kLength,BLOCKSIZE, seed=""):
+def keygen(kLength,BLOCKSIZE, seed="", randomness=True):
     """Generates a pseudorandom key of length kLength
     Uses a nonce to increase the output of the key to the required length
     -Assumes kLength is an integer>0
     -Assumes BLOCKSIZE is an integer>0
     -Assumes seed is convertable into a string
+    If randomness is True, the generated key is incremented with the urandom function output
     Returns a key as a String"""
     #initializes the key
     key="";
@@ -19,7 +20,11 @@ def keygen(kLength,BLOCKSIZE, seed=""):
         #encode is a method that allows encoding in specific formats, such as utf-8
         #hexdigest is a method that returns the data converted to hexadecimal (the first two characters returned are an indication of a hexadecimal number)
         #int converts the hexadecimal from base 16 to base 10
-        rawdata=str(urandom(BLOCKSIZE)).encode('utf-8')+(str(seed)+str(nonce)).encode('utf-8');
+        if (randomness==True):
+            randomdata=str(urandom(BLOCKSIZE)).encode('utf-8');
+        else:
+            randomdata="";
+        rawdata=randomdata+(str(seed)+str(nonce)).encode('utf-8');
         hashfunction=sha256(rawdata);
         key=hashfunction.hexdigest()[2:];
         nonce+=1;
