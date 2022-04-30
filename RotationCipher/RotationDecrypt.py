faad973ebe13c5479a30028e7716ecb53a69379c
RCHash <ricardocavanha@live.com>
2022-04-30 06:06:02 -0300

    WIP

--- RotationCipher/RotationDecrypt.py
+++ RotationCipher/RotationDecrypt.py
@@ -1,9 +1,11 @@
-def RotDecrypt(ciphertext):
+def RotDecrypt(ciphertext,wordListFileName):
     """
     Assumes the ciphertext is a string
     Breaks the cryptography if it is a simple rotation cipher
     Returns the best match for the plaintext
     """
+    #loads words from the dictionary
+    wordList=loadWords(wordListFileName);
     #cycle through the letters of the alphabet
     bestMatch=0;
     plaintext="";
@@ -36,8 +38,24 @@ def wordSeparator(text):
     #cycles through the letters
     for letter in range(0,len(text),1):
         if (letter>0):
+            raise NotImplementedError;
         #SEEK A BETTER WAY TO COMPARE
-        if (letter==" " or letter==" ")
+        #if (text[letter]==" " or text[letter]==" ")
 
-
-#testcode
+def loadWords(wordListFileName):
+    """
+    Assumes wordlist filename is a valid textfile with one word in each row
+    Converts the words in the file into a list of words
+    Returns the list of words
+    Depending on the word list size, this may take a while to complete
+    """
+    print("Loading word list from file")
+    # inFile: file
+    inFile = open(wordListFileName, 'r');
+    # wordList: list of strings
+    wordList = [];
+    for line in inFile:
+        wordList.append(line.strip().lower());
+    inFile.close();
+    print("  ", len(wordList), "words loaded.");
+    return wordList;
